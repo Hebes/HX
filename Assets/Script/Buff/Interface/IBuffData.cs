98ed26c98dbc25b35cf37f5b046b886e4fc6ed5f
Hebes <1607388033@qq.com>
2023-12-15 16:41:02 +0800

    11

---
+++ Assets/Script/Buff/Interface/IBuffData.cs
@@ -0,0 +1,18 @@
+﻿    public interface IBuffData
+    {
+        /// <summary>
+        /// Buff编号
+        /// </summary>
+        public int buffID { get; }
+
+        /// <summary>
+        /// buff名称
+        /// </summary>
+        public string buffName { get; }
+
+        /// <summary>
+        /// buf介绍
+        /// </summary>
+        public string describe { get; }
+
+    }
