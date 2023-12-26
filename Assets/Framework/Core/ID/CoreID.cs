60991e8d0ef0f34e0b23d97b767ec833ef416334
Hebes <1607388033@qq.com>
2023-12-26 17:18:39 +0800

    1

---
+++ Assets/Framework/Core/ID/CoreID.cs
@@ -0,0 +1,24 @@
+﻿namespace Core
+{
+    /// <summary>
+    /// 生成唯一ID
+    /// </summary>
+    public class CoreID : ICore
+    {
+        public static CoreID Instance;
+        private int m_id = 0;
+
+        public void ICoreInit()
+        {
+            Instance = this;
+        }
+
+        /// <summary>
+        /// 生成ID,唯一
+        /// </summary>
+        public static int GenerateID()
+        {
+            return Instance.m_id++;
+        }
+    }
+}
