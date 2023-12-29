4a0101d3f6251b8502ec73ad008c3eb4b386af02
Hebes <1607388033@qq.com>
2023-12-29 13:01:23 +0800

    1111

---
+++ Assets/Script/ModelStory/InterfaceStory.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// 故事的接口（PS:就是剧情）
+/// </summary>
+public interface IStory : IID
+{
+
+}
+
+/// <summary>
+/// 故事的生命周期
+/// </summary>
+public interface IStoryBehaviour : IStory
+{
+    public void Trigger();
+    public void Over();
+}
