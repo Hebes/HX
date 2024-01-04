c57f6892fcec40191cc6a2d0cff1932b9a572ad7
Hebes <1607388033@qq.com>
2024-01-04 23:46:01 +0800

    11

--- Assets/Script/ModelTask/InterfaceTask.cs
+++ Assets/Script/ModelTask/InterfaceTask.cs
@@ -3,16 +3,6 @@
 public interface ITask : IID, IName, IDescribe
 {
     /// <summary>
-    /// 任务名称
-    /// </summary>
-    //public string TaskName { get; set; }
-
-    /// <summary>
-    /// 任务说明
-    /// </summary>
-    //public string TaskDescription { get; set; }
-
-    /// <summary>
     /// 任务类型
     /// </summary>
     public EnumTask taskType { get; set; }
@@ -21,7 +11,13 @@ public interface ITask : IID, IName, IDescribe
     /// 任务进行的状态
     /// </summary>
     public EnumTaskStatus taskStatus { get; set; }
+}
 
+/// <summary>
+/// 任务的生命周期
+/// </summary>
+public interface ITaskBehaviour : IID
+{
     /// <summary>
     /// 任务触发
     /// </summary>
@@ -34,14 +30,6 @@ public interface ITask : IID, IName, IDescribe
 }
 
 /// <summary>
-/// 
-/// </summary>
-public interface ITaskBehaviour
-{
-
-}
-
-/// <summary>
 /// 任务的持有者 每个NPC或者团队必须继承这个才有任务
 /// </summary>
 public interface ITaskCarrier : IID
@@ -50,4 +38,22 @@ public interface ITaskCarrier : IID
     /// 任务列表
     /// </summary>
     public List<ITask> TaskList { get; set; }
+
+    public static void AddTask(ITaskCarrier taskCarrier , ITask task)
+    {
+        if (taskCarrier.TaskList==null)
+            taskCarrier.TaskList = new List<ITask>();
+        taskCarrier.TaskList.Add(task);
+    }
+
+    public static void RemoveTask(ITaskCarrier taskCarrier, ITask task)
+    {
+        taskCarrier.TaskList.Remove(task);
+    }
+}
+
+public static class HelperTask
+{
+    public static void AddTask(this ITaskCarrier taskCarrier, ITask task) => ITaskCarrier.AddTask(taskCarrier, task);
+    public static void RemoveTask(this ITaskCarrier taskCarrier, ITask task) => ITaskCarrier.RemoveTask(taskCarrier, task);
 }
