39e4a9543e0c750d7184b099289ede2b880c840b
Hebes <1607388033@qq.com>
2024-01-05 08:41:54 +0800

    画布管理

---
+++ Assets/Framework/Expansion/ExpansionUnity/ExpansionProfiler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Profiling;
+
+namespace Core
+{
+    public class ExpansionProfiler
+    {
+        /// <summary>
+        /// 需要成对出现
+        /// </summary>
+        /// <param name="str"></param>
+        public void ProfilerBeginSample(string str)
+        {
+            Profiler.BeginSample(str);
+        }
+
+        public void ProfilerEndSample()
+        {
+            Profiler.EndSample();
+        }
+    }
+}
---
+++ Assets/Script/ModelScreen/ManagerScreen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// https://cloud.tencent.com/developer/article/1637553
+/// https://blog.csdn.net/qq_33795300/article/details/131700773
+/// </summary>
+public class ManagerScreen : IModelInit
+{
+    public static ManagerScreen Instance;
+    private Resolution[] resolutions;
+    public void Init()
+    {
+        Instance = this;
+#if UNITY_STANDALONE_WIN
+        // 获取设置当前屏幕分辩率
+        resolutions = Screen.resolutions;
+
+        //设置当前分辨率
+        Screen.SetResolution(resolutions[resolutions.Length - 1].width, resolutions[resolutions.Length - 1].height, true);
+        Screen.fullScreen = true; //设置成全屏,
+#endif
+    }
+}
