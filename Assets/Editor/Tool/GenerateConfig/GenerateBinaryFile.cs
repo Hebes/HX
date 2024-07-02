0470b1a3573213248d4c342de32de32e67678554
Hebes <1607388033@qq.com>
2024-07-02 10:54:32 +0800

    11

---
+++ Assets/Editor/Tool/GenerateConfig/GenerateBinaryFile.cs
@@ -0,0 +1,10 @@
+﻿namespace ACEditor
+{
+    /// <summary>
+    /// 生成二进制数据文件
+    /// </summary>
+    public class GenerateBinaryFile
+    {
+        
+    }
+}
--- Assets/Framework/Core/06UI/Data/UIComponent.cs
+++ Assets/Framework/Core/06UI/Data/UIComponent.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-using Object = UnityEngine.Object;//Object并非C#基础中的Object，而是 UnityEngine.Object
+using Object = UnityEngine.Object; //Object并非C#基础中的Object，而是 UnityEngine.Object
 
 
 /*--------脚本描述-----------
@@ -25,24 +25,17 @@ namespace Core
     {
         public List<UIData> dataList = new List<UIData>();
 
-        public T Get<T>(string key) where T : class
+        public GameObject Get(string key)
         {
-            foreach (UIData data in dataList)
+            foreach (var data in dataList)
             {
-                if (data.key == key)
-                    return data.gameObject as T;
+                if (data.key != key) continue;
+                return data.gameObject as GameObject;
             }
-            return null;
+
+            return default;
         }
 
-        public T GetComponent<T>(string key) where T : Component
-        {
-            foreach (UIData data in dataList)
-            {
-                if (data.key == key)
-                    return (data.gameObject as GameObject).GetComponent<T>();
-            }
-            return null;
-        }
+        public T GetComponent<T>(string key) where T : Component => Get(key)?.GetComponent<T>();
     }
 }
